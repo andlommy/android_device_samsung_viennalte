6d74ea5e70efc06925cb4b2776e553f6c65404a4
andlommy <others.andrew@lomak.in>
2018-04-04 21:02:08 +0100

    Duplicated function, as it's referenced in qcom/power
    created power_set_interactive as it's referenced in qcom/power/power_helper.c

--- power/power_ext.c
+++ power/power_ext.c
@@ -52,3 +52,8 @@ void cm_power_set_interactive_ext(int on) {
     sysfs_write(TK_POWER, on ? "1" : "0");
     sysfs_write(TS_POWER, on ? "1" : "0");
 }
+void power_set_interactive_ext(int on) {
+    ALOGD("%s: %s input devices", __func__, on ? "enabling" : "disabling");
+    sysfs_write(TK_POWER, on ? "1" : "0");
+    sysfs_write(TS_POWER, on ? "1" : "0");
+}
